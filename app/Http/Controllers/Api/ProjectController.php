40953d638b939fe486be3331f5c136afbfa7f2dd
nicolo fossati <nicofossa01@gmail.com>
2023-05-30 14:26:59 +0200

    MILESTONE 1 e 2

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
