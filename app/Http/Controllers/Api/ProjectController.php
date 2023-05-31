128d20803be6985fac5a1583ccf74df5bd1f90eb
nicolo fossati <nicofossa01@gmail.com>
2023-05-31 16:51:07 +0200

    ESERCIzIO TERMINATO

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,12 +10,26 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(4);
+        //$projects = Project::all();
 
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
+
+    }
+
+    public function show($slug)
+    {
+
+        $project = Project::where('slug', $slug)->first();
+
+        return response()->json([
+            'success' => true,
+            'project' => $project
+        ]);
+
 
     }
 }
